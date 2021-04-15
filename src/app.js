acb8530f913656a91319023c1b71401838902d74
Richard H Dobeck <rdobeck@yahoo.com>
2021-04-15 12:51:37 -0700

    imported directory error, attached to app at / route

--- src/app.js
+++ src/app.js
@@ -10,12 +10,15 @@ const cors = require("cors");
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
+const directory = require("./errors/directory");
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
 app.use("/theaters", theatersRouter);
+app.use("/", directory);
 
 app.use(notFound);
 app.use(errorHandler);
