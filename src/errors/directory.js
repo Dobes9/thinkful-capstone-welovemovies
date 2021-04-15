1f5b4ba5213f7f41a971c83cadc08fb86c6ceb6e
Richard H Dobeck <rdobeck@yahoo.com>
2021-04-15 12:50:56 -0700

    created directory error for / route

---
+++ src/errors/directory.js
@@ -0,0 +1,10 @@
+/* eslint-disable strict */
+
+function directory(req, res, next) {
+  next({
+    error: 404,
+    message: `No content for /.  Try /movies, /movies/:movieId, /movies/:movieId/reviews, /movies/:movieId/theaters, theaters`,
+  });
+}
+
+module.exports = directory;
